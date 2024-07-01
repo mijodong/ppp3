24a441de8cbc7b8a85e1fe02352a9a88c791d188
mjdn <mijodong@users.noreply.github.com>
2024-06-30 21:17:39 -0500

    Add exercise 2

---
+++ chapter-01/exercise-02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Walk to the stairs.\n";
+    std::cout << "Walk up the stairs.\n";
+    std::cout << "Turn clockwise 90 degrees.\n";
+    std::cout << "Walk straight until the first door on the right.\n";
+    std::cout << "If the door is closed, open the door.\n";
+    std::cout << "Walk into the bathroom.\n";
+}
