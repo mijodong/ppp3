758f738a3c2187bb01bfb583711f84b13022d856
mjdn <mijodong@users.noreply.github.com>
2024-07-08 01:26:58 -0500

    Add Try This exercise for Chapter 02

---
+++ chapter-02/trythis-01.cpp
@@ -0,0 +1,10 @@
+#include "../PPPheaders.h"
+
+int main()
+{
+    cout << "Please enter your first and age:\n";
+    string first_name = "???"; // striing variable ("???" indicates "don't know the name")
+    double age = -1;           // double variable (-1 means "don't know the age")
+    cin >> first_name >> age;  // read a string followed by a double
+    cout << "Hello, " << first_name << " (age " << age * 12 << " months).\n";
+}
