f56532048847f097bcca684e534ff27940a1c118
mjdn <mijodong@users.noreply.github.com>
2024-07-17 23:52:45 -0500

    Add Chapter 2 Try This 5

---
+++ chapter-02/trythis-05.cpp
@@ -0,0 +1,15 @@
+#include "../PPPheaders.h"
+
+int main()
+{
+    double d = 0;
+    while (cin >> d)
+    {                                   // repeat the statements below as long as we type in numbers
+        int i = d;                      // try to squeeze a floating-point value into an integer value
+        char c = i;                     // try to squeeze an integer into a char
+        cout << "d==" << d              // the original double
+             << " i==" << i             // double converted to int
+             << " c==" << c             // int value of char
+             << " char(" << c << ")\n"; // the char
+    }
+}
