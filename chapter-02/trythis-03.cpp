f92c9c219215a570fb6cc4d70040e509a250471a
mjdn <mijodong@users.noreply.github.com>
2024-07-17 21:56:51 -0500

    Add Chapter 02 Try This 3

---
+++ chapter-02/trythis-03.cpp
@@ -0,0 +1,13 @@
+#include "../PPPheaders.h"
+
+int main()
+{
+    string previous;       // previous word; initalized to ""
+    string current;        // current word
+    while (cin >> current) // read a stream of words
+    {
+        if (previous == current) // check if the word is the same as last
+            cout << "repeated word: " << current << '\n';
+        previous = current;
+    }
+}
