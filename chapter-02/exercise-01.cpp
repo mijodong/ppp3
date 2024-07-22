734d889ec53c42d995f824f61e58c72160bb7518
mjdn <mijodong@users.noreply.github.com>
2024-07-21 21:30:02 -0500

    Add exercise 1

---
+++ chapter-02/exercise-01.cpp
@@ -0,0 +1,10 @@
+#include "../PPPheaders.h"
+
+int main()
+{
+    double miles;
+
+    cout << "Please enter the number of miles you want to convert to kilometers: ";
+    cin >> miles;
+    cout << miles << " mile(s) is equal to " << miles * 1.609 << " kilometer(s).";
+}
