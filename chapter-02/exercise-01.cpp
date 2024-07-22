fb4cb456fec05c7972c20eb0245acb4adb45c663
mjdn <mijodong@users.noreply.github.com>
2024-07-21 21:31:01 -0500

    Add return 0 to exercise 1

--- chapter-02/exercise-01.cpp
+++ chapter-02/exercise-01.cpp
@@ -7,4 +7,6 @@ int main()
     cout << "Please enter the number of miles you want to convert to kilometers: ";
     cin >> miles;
     cout << miles << " mile(s) is equal to " << miles * 1.609 << " kilometer(s).";
+
+    return 0;
 }
