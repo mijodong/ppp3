58e6575844af02ec28af48b66f92182140e3e204
mjdn <mijodong@users.noreply.github.com>
2024-08-08 22:26:40 -0500

    Add Chapter 3 Drill 7

--- chapter-03/drills.cpp
+++ chapter-03/drills.cpp
@@ -2,26 +2,81 @@
 
 int main()
 {
-    // Drill 6
+    // Drill 7
+    constexpr double cm_per_m = 100;
+    constexpr double cm_per_in = 2.54;
+    constexpr double in_per_ft = 12;
+
     double i;
     double smallest = 999999999999999;
     double largest = -999999999999999;
+    double i_as_m;
+    double smallest_as_m;
+    double largest_as_m;
+    string uom;
+    string smallest_uom;
+    string largest_uom;
 
-    while (cin >> i)
+    while (cin >> i >> uom)
     {
-        cout << i;
-        if (i < smallest)
+        cout << i << " " << uom;
+
+        // convert input to m as a base comparison value
+        if (uom == "cm")
+        {
+            i_as_m = i / cm_per_m;
+        }
+        else if (uom == "in")
+        {
+            i_as_m = i * cm_per_in / cm_per_m;
+        }
+        else if (uom == "ft")
+        {
+            i_as_m = i * in_per_ft * cm_per_in / cm_per_m;
+        }
+        else
+        {
+            i_as_m = i;
+        }
+
+        // check if smallest
+        if (smallest_uom != "")
+        {
+            if (i_as_m < smallest_as_m)
+            {
+                smallest = i;
+                smallest_uom = uom;
+                smallest_as_m = i_as_m;
+                cout << " is the smallest so far";
+            }
+        }
+        else
         {
             smallest = i;
+            smallest_uom = uom;
+            smallest_as_m = i_as_m;
             cout << " is the smallest so far";
         }
-        if (i > largest)
+
+        // check if largest
+        if (largest_uom != "")
         {
-            if (i == smallest)
-                cout << " and also";
-            largest = i;
-            cout << " is the largest so far";
+            if (i_as_m > largest_as_m)
+            {
+                largest = i;
+                largest_uom = uom;
+                largest_as_m = i_as_m;
+                cout << " is the largest so far";
+            }
         }
+        else
+        {
+            largest = i;
+            largest_uom = uom;
+            largest_as_m = i_as_m;
+            cout << " and is also the largest so far";
+        }
+
         cout << '\n';
     }
 
