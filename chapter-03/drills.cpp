3718690ae0fa3a1ea6ce8395697a034ae486cfe4
mjdn <mijodong@users.noreply.github.com>
2024-08-08 21:39:22 -0500

    Complete Chapter 3 Drill 5

--- chapter-03/drills.cpp
+++ chapter-03/drills.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // Drill 4
+    // Drill 5
     double i, j;
     while (cin >> i >> j)
     {
@@ -10,11 +10,15 @@ int main()
         {
             cout << "The smaller value is: " << i << '\n';
             cout << "The larger value is: " << j << '\n';
+            if (abs(j - i) < 1.0 / 100)
+                cout << "The numbers are almost equal!" << '\n';
         }
         else if (j < i)
         {
             cout << "The smaller value is: " << j << '\n';
             cout << "The larger value is: " << i << '\n';
+            if (abs(i - j) < 1.0 / 100)
+                cout << "The numbers are almost equal!" << '\n';
         }
         else
         {
