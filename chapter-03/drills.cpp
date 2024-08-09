324ef4f8e12581b32a8cc0e5d4116a099a1e5de3
mjdn <mijodong@users.noreply.github.com>
2024-08-08 21:33:50 -0500

    Complete Chapter 3 Drill 3

--- chapter-03/drills.cpp
+++ chapter-03/drills.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // Drill 2
+    // Drill 3
     int i, j;
     while (cin >> i >> j)
     {
@@ -11,10 +11,14 @@ int main()
             cout << "The smaller value is: " << i << '\n';
             cout << "The larger value is: " << j << '\n';
         }
-        else
+        else if (j < i)
         {
             cout << "The smaller value is: " << j << '\n';
             cout << "The larger value is: " << i << '\n';
+        }
+        else
+        {
+            cout << "The inputs are equal." << '\n';
         }
     }
 
